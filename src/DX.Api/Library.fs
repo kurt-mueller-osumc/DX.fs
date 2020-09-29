ec71e8c40fd240fc6a62ff1737221e9883a347e7
Kurt Mueller <kurt.mueller@osumc.edu>
2020-09-29 11:23:16 -0400

    Find data objects

--- src/DX.Api/Library.fs
+++ src/DX.Api/Library.fs
@@ -4,53 +4,49 @@ open FSharp.Data
 open FSharp.Data.HttpRequestHeaders
 open Newtonsoft.Json
 
-module Response =
-    module Error =
-        /// An error response returned by the DNAnexus api
-        type Json = 
-            { error: {| ``type`` : string
-                        message : string |} }
-        
-        /// Parse an error response into json
-        let parseJson json =
-            JsonConvert.DeserializeObject<Json>(json) 
+module Base =
+    module Response =
+        module Error =
+            /// An error response returned by the DNAnexus api
+            type Json = 
+                { error: {| ``type`` : string
+                            message : string |} }
+            
+            /// Parse an error response into json
+            let parseJson json =
+                JsonConvert.DeserializeObject<Json>(json) 
 
-    /// A response from DNAnexus API
-    type Response<'a> =
-        | InvalidAuthentication of Error.Json
-        | ResourceNotFound of Error.Json
-        | InvalidResponseType of string
-        | Unknown of string
-        | Valid of 'a
+        /// A response from DNAnexus API
+        type Response<'a> =
+            | InvalidAuthentication of Error.Json
+            | ResourceNotFound of Error.Json
+            | InvalidResponseType of string
+            | Unknown of string
+            | Valid of 'a
 
-    /// Parse a response from the DNAnexus API
-    let parse parseFn (response:HttpResponse) =
-        match (response.StatusCode, response.Body) with
-        | (401, Text errorJson) -> InvalidAuthentication (Error.parseJson errorJson)
-        | (404, Text errorJson) -> ResourceNotFound (Error.parseJson errorJson)
-        | (200, Text jsonText) -> Valid (parseFn jsonText)
-        | (_ , Text responseText) -> Unknown responseText
-        | (_, Binary bytes) -> InvalidResponseType ("Binary response: " + System.Text.Encoding.ASCII.GetString(bytes))
+        /// Parse a response from the DNAnexus API
+        let parse parseFn (response:HttpResponse) =
+            match (response.StatusCode, response.Body) with
+            | (401, Text errorJson) -> InvalidAuthentication (Error.parseJson errorJson)
+            | (404, Text errorJson) -> ResourceNotFound (Error.parseJson errorJson)
+            | (200, Text jsonText) -> Valid (parseFn jsonText)
+            | (_ , Text responseText) -> Unknown responseText
+            | (_, Binary bytes) -> InvalidResponseType ("Binary response: " + System.Text.Encoding.ASCII.GetString(bytes))
 
-module Common =
     /// The token used to access the DNAnexus API
     type ApiToken = ApiToken of string
 
-module Request =
-    open Common
-
-    let headers (ApiToken apiToken) =
-        [ ContentType HttpContentTypes.Json
-          "Authorization", ("Bearer " + apiToken) ]
+    module Request =
+        let headers (ApiToken apiToken) =
+            [ ContentType HttpContentTypes.Json
+              "Authorization", ("Bearer " + apiToken) ]
 
 module DataObjects =
-    open Common
-
     type ProjectId = ProjectId of string
     type ObjectId = ObjectId of string
 
     type Request = {
-        ApiToken: ApiToken
+        ApiToken: Base.ApiToken
         ProjectId: ProjectId
         StartingAt: ObjectId option
     }
@@ -72,15 +68,15 @@ module DataObjects =
                 | None ->
                     JsonConvert.SerializeObject(baseJson)
         
-        let request request =
+        let initiate request =
             Http.Request
                 ( "https://api.dnanexus.com/system/findDataObjects",
                   httpMethod = "POST",
                   silentHttpErrors = true,
-                  headers = Request.headers request.ApiToken,
+                  headers = Base.Request.headers request.ApiToken,
                   body = TextRequest (serializeBody request))
 
-    module Reponse =
+    module Response =
         type DataObjectJson = {
             project: string
             id: string
@@ -107,12 +103,18 @@ module DataObjects =
         }
 
         /// The json repsonse if the API call is successful
-        type Json = {
+        type BodyJson = {
             /// The next file id to start the search at
             next: {| project: string
                      id: string |}
             results: DataObjectJson list
         }
+ 
+        let deserializeBody jsonText =
+            JsonConvert.DeserializeObject<BodyJson>(jsonText)
+        
+        let parse =
+            Base.Response.parse deserializeBody
 
-        let deserialize jsonText =
-            JsonConvert.DeserializeObject<Json>(jsonText)
+    let find = 
+        Request.initiate >> Base.Response.parse Response.deserializeBody
